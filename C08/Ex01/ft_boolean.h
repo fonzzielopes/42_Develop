e1a0e796269a58f64b4b2ac04ca20c9cfee285cd
fonzzielopes <afonsolopes1992@hotmail.com>
2021-01-30 19:24:22 +0000

    3

---
+++ C08/Ex01/ft_boolean.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alopes- <alopes-@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/30 00:04:17 by alopes-           #+#    #+#             */
+/*   Updated: 2021/01/30 17:57:27 by alopes-          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef enum {
+	true = 1,
+	false = 0
+}	t_bool;
+
+# define TRUE 1
+# define FALSE 0
+# define EVEN(N) (N % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+#endif
