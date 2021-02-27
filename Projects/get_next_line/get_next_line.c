811585faa9fe3c8d1fef981dcd2ecba2cf117726
Afonso Lopes <alopes@c1r15s15.42lisboa.com>
2021-02-27 15:40:13 +0000

    3

--- Projects/get_next_line/get_next_line.c
+++ Projects/get_next_line/get_next_line.c
@@ -6,57 +6,11 @@
 /*   By: alopes <alopes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 16:03:35 by alopes            #+#    #+#             */
-/*   Updated: 2021/02/27 14:06:08 by alopes           ###   ########.fr       */
+/*   Updated: 2021/02/27 15:39:17 by alopes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-/*
-static int archive (char **saved, *line);
-
-static int	result(char **saved, char **line, int ret, int fd)
-{
-	if (ret < 0)
-		return (-1);
-	else if (ret == 0 && saved[fd] == NULL)
-		return (0);
-	else
-		return (archive(&saved[fd], line));
-}
-
-int	get_next_line(int fd, char **line)
-{
-	char buf[BUFFER_SIZE + 1];
-	static char *saved[1024];
-	char *tmp;
-	int ret;
-
-	if (fd < 0 || line == NULL)
-		return (-1);
-	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0)
-	{
-		buf[ret] = '\0';
-        if (saved[fd] == NULL)
-            saved[fd] = ft_strdup(buf);
-		else
-		{
-			tmp = ft_strjoin(saved[fd], buf);
-			free(saved[fd]);
-			saved[fd] = tmp;
-		}
-		if (ft_strchr(saved[fd], '\n'))
-			break ;
-	}
-	return (result(saved, line, ret, fd));
-}*/
-
-/*
-** Con esta funcion buscamos las lineas y las guardamos una a una.
-** 1. Chequeamos el stack buscando la primera linea.
-** 2. Hacemos un substr guardando la linea.
-** 3. Hacemos copia de stack en temp y duplicamos la linea con strdup en stack.
-** 4. Liberamos temp y devolvemos 1 si ha ido bien.
-*/
 
 static int	archive(char **saved, char **line)
 {
@@ -64,8 +18,8 @@ static int	archive(char **saved, char **line)
 
 	if (ft_strchr(*saved, '\n'))
 	{
-		*line = ft_substr(*saved, 0, ft_strlen(*saved) -
-				ft_strlen(ft_strchr(*saved, '\n')));
+		*line = ft_substr(*saved, 0, ft_strlen(*saved)
+				- ft_strlen(ft_strchr(*saved, '\n')));
 		temp = *saved;
 		*saved = ft_strdup(ft_strchr(temp, '\n') + 1);
 		free(temp);
@@ -76,26 +30,25 @@ static int	archive(char **saved, char **line)
 }
 
 /*
-** Esta funcion lee las lineas del archivo
-** 1. Alocamos memoria para buf
-** 2. Mientras que haya texto, lee y une el stack con el buf. Luego libera.
-** 3. Si llega al final del texto hace un strdup de buf.
-** 4. Si llega al final de linea, rompe el bucle.
-** Devuelve el numero de bytes leidos.
+** 1. Alocar memoria para buf
+** 2. Enquanto ha texto, le e junta o saved ao buf, depois liberta (free)
+** 3. Quando chega ao final do texto, copia o buf
+** 4. Se chega a "\n", faz break
+** Devolve o tamanho.
 */
 
 static int	read_line(int fd, char **saved)
 {
 	char	*buf;
-	int		bytes;
+	int		size;
 	char	*temp;
 
 	buf = malloc(sizeof(char) * (BUFF_SIZE + 1));
 	if (!buf)
 		return (-1);
-	while ((bytes = read(fd, buf, BUFF_SIZE)) > 0)
+	while ((size = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		buf[bytes] = '\0';
+		buf[size] = '\0';
 		if (saved[fd])
 		{
 			temp = saved[fd];
@@ -108,25 +61,32 @@ static int	read_line(int fd, char **saved)
 			break ;
 	}
 	free(buf);
-	return (bytes);
+	return (size);
 }
+
+/*
+** 1. Verificar situacoes de erro
+** 2. Verificar primeira linha, enviar para o archivo e verificar ate encontrar "\n"
+** 3. Se a linha que leu for 0, devolve EOF
+** 4. Se a linha que leu tiver conteudo, volta a enviar para o archive
+** 5. Se nao tiver chegado a uma "\n", copiar o conteudo para line, e libertar o saved. EOF
+**
+*/
 
 int			get_next_line(int fd, char **line)
 {
 	static char	*saved[1024];
 	int			ret;
-	char		buf[BUFF_SIZE + 1];
 
-	if (fd < 0 || line == NULL || read(fd, saved, 0) < 0)
+	if (fd < 0 || line == NULL || read(fd, 0, 0) < 0)
 		return (-1);
 	if (saved[fd])
 		if (archive(&saved[fd], line))
 			return (1);
 	ret = read_line(fd, saved);
-	buf[ret] = '\0';
 	if (ret == 0 && !saved[fd])
 	{
-		*line = ft_strdup(buf);
+		*line = ft_strdup(NULL);
 		return (0);
 	}
 	if (archive(&saved[fd], line))
