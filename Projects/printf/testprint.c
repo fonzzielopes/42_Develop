d2a8dc15a92bb75faba927f06ad54f23849edb39
Afonso Lopes <alopes@c1r18s7.42lisboa.com>
2021-03-11 16:57:15 +0000

    p3

--- Projects/printf/testprint.c
+++ Projects/printf/testprint.c
@@ -1,55 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   testprint.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alopes <alopes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/10 12:54:48 by alopes            #+#    #+#             */
-/*   Updated: 2021/03/11 16:53:50 by alopes           ###   ########.fr       */
+/*   Created: 2021/03/11 16:44:56 by alopes            #+#    #+#             */
+/*   Updated: 2021/03/11 16:45:20 by alopes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 #include "libft/libft.h"
 
-t_flags	ft_init_flags(void)
-{
-	t_flags flags;
-	flags.dot = 0;
-	flags.star = 0;
-	flags.minus = 0;
-	flags.zero = 0;
-	flags.width = 0;
-	flags.type = 0;
-	flags.min_width = 0;
-
-	return(flags);
-}
-
-int	check_input(const char *input, va_list args)
-{
-	int i;
-	t_flags flags;
-	int count;
-
-	i = 0;
-	count = 0;
-	flags = ft_init_flags();
-
-	return (count);
-}
-
 int ft_printf(const char *format, ...)
 {
 	va_list args;
 	int count;
-	const char *input;
+	char *input;
+	char *temp;
 
 	input = ft_strdup((char *)format);
 	count = 0;
 	va_start(args, format);
-	count += check_input(input, args);
+	while(input && input[count])
+	{
+		if (input[count] != '%')
+			ft_putchar(input[count]);
+		else
+		{
+			count++;
+			if (input[count] == 's')
+			{
+				temp = va_arg(args, char *);
+				ft_putstr(temp);
+			}
+		}
+		count++;
+	}
 	va_end(args);
 	free((char *)input);
 	return (count);
