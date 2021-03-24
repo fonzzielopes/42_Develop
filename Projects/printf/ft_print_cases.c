f9d11cc17971e5647ad542638a10b8b179447d8c
Afonso Lopes <alopes@c1r10s6.42lisboa.com>
2021-03-24 15:16:10 +0000

    24/03

--- Projects/printf/ft_print_cases.c
+++ Projects/printf/ft_print_cases.c
@@ -6,7 +6,7 @@
 /*   By: alopes <alopes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 13:12:27 by alopes            #+#    #+#             */
-/*   Updated: 2021/03/23 13:56:56 by alopes           ###   ########.fr       */
+/*   Updated: 2021/03/24 14:58:34 by alopes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ static t_flags	width_spec(t_flags flag, int *len)
 		flag.minus = 1;
 	if (flag.dot && *len > flag.precision && flag.type == 's')
 	/* tem que ter '.', comprimento da str nao pode ser menor que a precision para depois ser definida como arg principal
-	tem de ser tambem uma string, pois para caso de interger nao vai ler o ultimo arg */
+	tem de ser tambem uma string, pois apos definiçao de precision ele vai ignorar os args passados */
 		*len = flag.precision; // definimos o valor max a ser impresso como len
 	return (flag);
 }
@@ -91,11 +91,11 @@ int				case_ptr(unsigned long *ptr, t_flags flag)
 	int		prec;
 
 	count = 0;
-	prec = flag.precision;
-	arr = ft_itoa_hex(ptr, prec);
+	prec = flag.precision; //precisamos de saber a prec para verificar o padding de 0's necessario
+	arr = ft_itoa_hex(ptr, prec); // feito nesta conversao do ptr para str
 	if (arr == NULL)
 		return (-1);
 	count += case_str(arr, flag);
-	free(arr);
+	free(arr); // proteger de leaks
 	return (count);
 }
--- Projects/printf/ft_print_nbr.c
+++ Projects/printf/ft_print_nbr.c
@@ -6,24 +6,26 @@
 /*   By: alopes <alopes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 16:33:42 by alopes            #+#    #+#             */
-/*   Updated: 2021/03/23 15:16:39 by alopes           ###   ########.fr       */
+/*   Updated: 2021/03/24 14:58:13 by alopes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static t_flags		get_flags(t_flags flag, int *prec)
+static t_flags		get_flags(t_flags flag, int *prec, int nbr)
 {
-	if ((flag.dot || flag.width < 0 || flag.minus) && flag.zero)
-		flag.zero = 0;
+	if (((flag.dot && *prec >= 0) || flag.width < 0 || flag.minus) && flag.zero)
+		flag.zero = 0; // todos os casos para que a flag.zero seja ignorada
 	if (flag.zero)
-	{
+	{ // ter atençao que flag.zero e ter padding é diferente, pois na flag é necessario contabilizar
+	// a width, pois tem de continuar a ser impressa, daí termos que equilizar a nossa prec com width
 		*prec = flag.width;
+		if (nbr < 0)
+			(*prec)--;
 		flag.width = 0;
 	}
 	return (flag);
 }
-
 
 int				case_nbr(int nbr, t_flags flag)
 {
@@ -32,8 +34,11 @@ int				case_nbr(int nbr, t_flags flag)
 	int		prec;
 
 	prec = flag.precision;
-	flag = get_flags(flag, &prec);
-	arr = ft_itoa_base(nbr, prec); //adiciona o padding de 0s
+	flag = get_flags(flag, &prec, nbr);
+	if (flag.dot && prec == 0 && nbr == 0 && flag.zero == 0) // todos os casos para devolver nada
+		arr = ft_strdup("");
+	else
+		arr = ft_itoa_base(nbr, prec); // igual ao ptr, converte em str e adiciona o padding de 0's
 	if (arr == NULL)
 		return (-1);
 	count = case_str(arr, flag);
