c973f23e028cb491ffd66715729442111636e54b
Afonso Lopes <alopes@c1r9s6.42lisboa.com>
2021-04-08 16:56:49 +0100

    08/04

--- Projects/ft_server/srcs/init.sh
+++ Projects/ft_server/srcs/init.sh
@@ -1,42 +1,41 @@
-#!/bin/bash
-
-service php7.3-fpm start
-
-#config access
-# Giving nginx's user-group rights over page files
-chown -R www-data /var/www/*
-chmod -R 755 /var/www/*
-
-# Generate website folder
-mkdir /var/www/localhost && touch /var/www/localhost/index.php
-echo "<?php phpinfo(); ?>" >> /var/www/localhost/index.php
-
-#SSL certificate
-mkdir /etc/nginx/ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-	-subj '/C=PT/ST=LS/L=Lisboa/O=42Lisboa/CN=alopes' \
-	-keyout /etc/ssl/certs/localhost.key -out /etc/ssl/certs/localhost.crt
-
-#SQL config
 service mysql start
+service php7.3-fpm start
+service nginx start
+
+
+# NGINX
+        rm var/www/html/index.nginx-debian.html
+		cp srcs/nginx.conf etc/nginx/sites-available/html
+		ln -s etc/nginx/sites-available/html etc/nginx/sites-enabled/html
+		rm -rf /etc/nginx/sites-enabled/default
+
+# SLL
+mkdir ~/mkcert && cd ~/mkcert && \
+	wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64 && \
+	mv mkcert-v1.4.3-linux-amd64 mkcert && chmod +x mkcert && \
+	./mkcert -install && ./mkcert html
+	cd /
+
 echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;"| mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
 echo "update mysql.user set plugin='' where user='root';"| mysql -u root --skip-password
-service nginx start
 
-#PHP config 
-mkdir -p /var/www/localhost/phpmyadmin
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.1/phpMyAdmin-5.0.1-english.tar.gz
-tar -xf phpMyAdmin-5.0.1-english.tar.gz 
-mv /var/config.inc.php /var/www/localhost/phpmyadmin/
-rm -rf phpMyAdmin-5.0.1-english.tar.gz
+# PHPMYADMIN
+mkdir -p /var/www/html/phpmyadmin
+wget https://files.phpmyadmin.net/phpMyAdmin/5.1.0/phpMyAdmin-5.1.0-all-languages.tar.gz
+tar -xf phpMyAdmin-5.1.0-all-languages.tar.gz 
+mv config.inc.php /var/www/html/phpmyadmin/
+rm -rf phpMyAdmin-5.1.0-all-languages.tar.gz
 
-#Wordpress config
+# WORDPRESS
 wget https://wordpress.org/latest.tar.gz
-tar -xvzf latest.tar.gz
-mv wordpress/ /var/www/localhost
-mv /var/wp-config.php /var/www/localhost/wordpress
+tar -xzf latest.tar.gz
+mv wp-config.php /var/www/html/ 
 rm -rf latest.tar.gz
+
+# Giving nginx's user-group rights over page files
+chown -R www-data /var/www/*
+chmod -R 755 /var/www/*
 
 bash
--- Projects/ft_server/start_docker.sh
+++
@@ -1,4 +0,0 @@
-# File to start the docker script
-docker build -t ft_server . 
-docker run --rm --name myserver -d -p 80:80 -p 443:443 ft_server
-docker exec -it myserver bash
